2b2877ac9b8af4d0b042e886dccb6b8daacff659
Sennay <wsennay@gmail.com>
2022-04-19 21:07:53 +0300

    create get all health centers service

--- src/health-center/health-center.service.ts
+++ src/health-center/health-center.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class HealthCenterService {}
+export class HealthCenterService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllHealthCenters() {
+    return this.prisma.healthCenter.findMany();
+  }
+}
