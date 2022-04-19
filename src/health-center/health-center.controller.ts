ee06f0e3d82771838a328b7824e0d8f035a5f513
Sennay <wsennay@gmail.com>
2022-04-19 21:10:19 +0300

    create get all health centers controller

--- src/health-center/health-center.controller.ts
+++ src/health-center/health-center.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HealthCenterService } from './health-center.service';
 
 @Controller('health-center')
-export class HealthCenterController {}
+export class HealthCenterController {
+  constructor(private readonly healthCenterService: HealthCenterService) {}
+
+  @Get()
+  async getAllHealthCenters() {
+    return this.healthCenterService.getAllHealthCenters();
+  }
+}
