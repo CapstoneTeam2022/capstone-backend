ce9465d93345c5c8d2444e05d0271f0adf6bf3b8
Sennay <wsennay@gmail.com>
2022-04-19 21:13:08 +0300

    create service to make a new health center

--- src/health-center/health-center.service.ts
+++ src/health-center/health-center.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Post } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { HealthCenterDto } from './dto';
 
 @Injectable()
 export class HealthCenterService {
@@ -7,5 +8,22 @@ export class HealthCenterService {
 
   async getAllHealthCenters() {
     return this.prisma.healthCenter.findMany();
+  }
+
+  async createHealthCenter(body: HealthCenterDto) {
+    //check email unique here
+    const newHealthCenter = await this.prisma.healthCenter.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        type: body.type,
+        address: {
+          create: {
+            ...body.address,
+          },
+        },
+      },
+    });
+    return newHealthCenter;
   }
 }
