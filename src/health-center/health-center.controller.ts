550037b1fd145b487cede9a61e9b4c9b199b4a7d
Sennay <wsennay@gmail.com>
2022-04-19 21:22:22 +0300

    add create route

--- src/health-center/health-center.controller.ts
+++ src/health-center/health-center.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { HealthCenterDto } from './dto';
 import { HealthCenterService } from './health-center.service';
 
 @Controller('health-center')
@@ -8,5 +9,10 @@ export class HealthCenterController {
   @Get()
   async getAllHealthCenters() {
     return this.healthCenterService.getAllHealthCenters();
+  }
+
+  @Post()
+  createHealthCenter(@Body() dto: HealthCenterDto) {
+    return this.healthCenterService.createHealthCenter(dto);
   }
 }
