a03232c7fb373eab52521cfcc7761fc2333563d7
Sennay <wsennay@gmail.com>
2022-04-24 17:41:46 +0300

    add user dto

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,8 +1,54 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { AddressDto } from 'src/utils/dto';
+import { Type } from 'class-transformer';
+
 export class UserDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   phone: string;
+
+  @IsNotEmpty()
+  @IsInt()
   age: number;
+
+  @IsNotEmpty()
+  @IsString()
   gender: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  isAdmin: boolean;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  isResearcher: boolean;
+
+  @IsNotEmpty()
+  @IsString()
+  role: string;
+
+  @ValidateNested({ each: true })
+  @Type(() => AddressDto)
+  @IsNotEmpty()
+  address: AddressDto;
 }
