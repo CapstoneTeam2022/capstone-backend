bfcd024ac3b722921f7efac08369a8fccce523f7
Sennay <wsennay@gmail.com>
2022-04-24 17:27:56 +0300

    remove user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { HealthCenterModule } from './health-center/health-center.module';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, HealthCenterModule, UserModule],
+  imports: [PrismaModule, HealthCenterModule],
   controllers: [AppController],
   providers: [AppService],
 })
