bf20a7c395f609caa595327bdf6071b9b2ae4d1d
Sennay <wsennay@gmail.com>
2022-05-02 21:40:59 +0300

    Add UserService

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { AddressModule } from '../address/address.module';
+import { RoleModule } from '../role/role.module';
 
 @Module({
-  providers: [UserService]
+  providers: [UserService],
+  imports: [TypeOrmModule.forFeature([User]), AddressModule, RoleModule],
 })
 export class UserModule {}
