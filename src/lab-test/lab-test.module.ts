c118ad6356d5db399985aeca829cfa97fb47a5fd
Robel Shewangzaw <61440928+Robel-shewan@users.noreply.github.com>
2022-05-02 08:43:40 +0300

    minor change in update functionaies of lab test

--- src/lab-test/lab-test.module.ts
+++ src/lab-test/lab-test.module.ts
@@ -1,4 +1,12 @@
+import { LabTest } from './labTest.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { LabTestController } from './lab-test.controller';
+import { LabTestService } from './lab-test.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([LabTest])],
+  controllers: [LabTestController],
+  providers: [LabTestService],
+})
 export class LabTestModule {}
