9ad58a64f36264ac55eb45d5b13732741ed7f8c3
Sennay <wsennay@gmail.com>
2022-05-02 22:02:19 +0300

    Import UserModule

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PatientService } from './patient.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Patient } from './patient.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Patient])],
+  imports: [TypeOrmModule.forFeature([Patient]), UserModule],
   providers: [PatientService],
 })
 export class PatientModule {}
