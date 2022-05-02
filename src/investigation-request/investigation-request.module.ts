e08edc753e60a18c74e1d828fdc347eab0c510fe
Robel Shewangzaw <61440928+Robel-shewan@users.noreply.github.com>
2022-05-02 09:09:23 +0300

    Adding the creation of investigation request  information

--- src/investigation-request/investigation-request.module.ts
+++ src/investigation-request/investigation-request.module.ts
@@ -1,4 +1,12 @@
+import { InvestigationRequest } from './investigationRequest.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { InvestigationRequestController } from './investigation-request.controller';
+import { InvestigationRequestService } from './investigation-request.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([InvestigationRequest])],
+  controllers: [InvestigationRequestController],
+  providers: [InvestigationRequestService],
+})
 export class InvestigationRequestModule {}
