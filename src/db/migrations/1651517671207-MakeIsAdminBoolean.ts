3ada9d25ec3c8c9ae81c021cf114b418ee740a4c
Sennay <wsennay@gmail.com>
2022-05-02 21:56:25 +0300

    Lint

--- src/db/migrations/1651517671207-MakeIsAdminBoolean.ts
+++ src/db/migrations/1651517671207-MakeIsAdminBoolean.ts
@@ -1,16 +1,19 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class MakeIsAdminBoolean1651517671207 implements MigrationInterface {
-    name = 'MakeIsAdminBoolean1651517671207'
+  name = 'MakeIsAdminBoolean1651517671207';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isAdmin"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "isAdmin" boolean NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isAdmin"`);
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "isAdmin" boolean NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isAdmin"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "isAdmin" character varying NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isAdmin"`);
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "isAdmin" character varying NOT NULL`,
+    );
+  }
 }
