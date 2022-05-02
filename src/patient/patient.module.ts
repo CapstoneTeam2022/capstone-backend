9916b9095c32712c3dc101f4cd53ccd3f8001af7
Sennay <wsennay@gmail.com>
2022-05-02 21:48:10 +0300

    Add Patient Repo

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PatientService } from './patient.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Patient } from './patient.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Patient])],
+  providers: [PatientService],
+})
 export class PatientModule {}
