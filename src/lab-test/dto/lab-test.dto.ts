c14a28d0b4f3e5cb51cbba0db8ab541449c26bd1
Sennay <wsennay@gmail.com>
2022-05-03 11:56:58 +0300

    Create Lab Test Dto

---
+++ src/lab-test/dto/lab-test.dto.ts
@@ -0,0 +1,24 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class LabTestDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  normalRange: string;
+
+  @IsNotEmpty()
+  @IsString()
+  measuredIn: string;
+
+  @IsNotEmpty()
+  @IsString()
+  testCategory: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  investigationRequestId: number;
+}
