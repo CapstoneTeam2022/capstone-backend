27628f84f64d4f14814661c14446a9ff17c16f2a
Sennay <wsennay@gmail.com>
2022-05-03 11:42:42 +0300

    Add User and Vital Modules

--- src/investigation-request/investigation-request.module.ts
+++ src/investigation-request/investigation-request.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { InvestigationRequestService } from './investigation-request.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { InvestigationRequest } from './investigationRequest.entity';
+import { UserModule } from '../user/user.module';
+import { VitalsModule } from '../vitals/vitals.module';
 
 @Module({
   providers: [InvestigationRequestService],
-  imports: [TypeOrmModule.forFeature([InvestigationRequest])],
+  imports: [
+    TypeOrmModule.forFeature([InvestigationRequest]),
+    UserModule,
+    VitalsModule,
+  ],
 })
 export class InvestigationRequestModule {}
