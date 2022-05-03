d6064579c0b8b1bfb5cd191aa35afdf43896bc95
Sennay <wsennay@gmail.com>
2022-05-03 12:04:17 +0300

    Import Investigation Request Module

--- src/lab-test/lab-test.module.ts
+++ src/lab-test/lab-test.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LabTestService } from './lab-test.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LabTest } from './labTest.entity';
+import { InvestigationRequestModule } from '../investigation-request/investigation-request.module';
 
 @Module({
   providers: [LabTestService],
-  imports: [TypeOrmModule.forFeature([LabTest])],
+  imports: [TypeOrmModule.forFeature([LabTest]), InvestigationRequestModule],
 })
 export class LabTestModule {}
