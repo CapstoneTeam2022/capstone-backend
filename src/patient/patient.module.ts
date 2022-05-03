adccaae8e17ece5e4c9d400e54876d843e6bec36
Sennay <wsennay@gmail.com>
2022-05-03 07:42:37 +0300

    Add Patient Controller

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -3,9 +3,11 @@ import { PatientService } from './patient.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Patient } from './patient.entity';
 import { UserModule } from '../user/user.module';
+import { PatientController } from './patient.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Patient]), UserModule],
   providers: [PatientService],
+  controllers: [PatientController],
 })
 export class PatientModule {}
