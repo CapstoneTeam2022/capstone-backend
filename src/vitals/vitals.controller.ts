6852849fb5a57b9322a3a4892c5a6ca737d76e2a
Sennay <wsennay@gmail.com>
2022-05-03 08:56:38 +0300

    Add get, get all, and create

--- src/vitals/vitals.controller.ts
+++ src/vitals/vitals.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { VitalsService } from './vitals.service';
+import { VitalsDto } from './dto';
 
 @Controller('vitals')
-export class VitalsController {}
+export class VitalsController {
+  constructor(private vitalsService: VitalsService) {}
+
+  @Get()
+  getAll() {
+    return this.vitalsService.getAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.vitalsService.getVital(id);
+  }
+
+  @Post()
+  create(@Body() body: VitalsDto) {
+    return this.vitalsService.createVital(body);
+  }
+}
