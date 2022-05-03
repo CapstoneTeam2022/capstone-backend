1689b4d6a16ecdf7902809a5ac34a9677ea772fd
Sennay <wsennay@gmail.com>
2022-05-03 13:25:52 +0300

    Add Dto

---
+++ src/radiology/dto/radiology.dto.ts
@@ -0,0 +1,31 @@
+import { IsArray, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class RadiologyDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  focalArea: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  report: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  images: string[];
+
+  @IsNotEmpty()
+  @IsString()
+  comment: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  requestedById: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  investigationRequestId: number;
+}
