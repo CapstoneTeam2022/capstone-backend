a90ed396042f4401a638c7d63366d2a4d358a0d4
Sennay <wsennay@gmail.com>
2022-05-03 12:41:46 +0300

    Import User and Investigation Request Modules

--- src/lab-result/lab-result.module.ts
+++ src/lab-result/lab-result.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { LabResultService } from './lab-result.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LabResult } from './labResult.entity';
+import { UserModule } from '../user/user.module';
+import { InvestigationRequestModule } from '../investigation-request/investigation-request.module';
 
 @Module({
   providers: [LabResultService],
-  imports: [TypeOrmModule.forFeature([LabResult])],
+  imports: [
+    TypeOrmModule.forFeature([LabResult]),
+    UserModule,
+    InvestigationRequestModule,
+  ],
 })
 export class LabResultModule {}
