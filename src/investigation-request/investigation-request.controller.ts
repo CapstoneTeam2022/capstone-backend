f802b14c74c4695e66116b45ace43b2012a7d96f
Sennay <wsennay@gmail.com>
2022-05-03 11:57:38 +0300

    Create Get All, Get One and Create

--- src/investigation-request/investigation-request.controller.ts
+++ src/investigation-request/investigation-request.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { InvestigationRequestService } from './investigation-request.service';
+import { InvestigationRequestDto } from './dto';
 
 @Controller('investigation-request')
-export class InvestigationRequestController {}
+export class InvestigationRequestController {
+  constructor(private service: InvestigationRequestService) {}
+
+  @Get()
+  getAll() {
+    return this.service.getAll();
+  }
+
+  @Get('id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.service.getInvestigationRequest(id);
+  }
+
+  @Post()
+  create(@Body() body: InvestigationRequestDto) {
+    return this.service.createInvestigationRequest(body);
+  }
+}
