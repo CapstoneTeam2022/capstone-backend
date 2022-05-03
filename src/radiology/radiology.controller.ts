2f347355eef748067184f0c60a9e1d587a9f10e4
Sennay <wsennay@gmail.com>
2022-05-03 13:53:04 +0300

    Add Get One, Get All and Create

--- src/radiology/radiology.controller.ts
+++ src/radiology/radiology.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { RadiologyService } from './radiology.service';
+import { RadiologyDto } from './dto';
 
 @Controller('radiology')
-export class RadiologyController {}
+export class RadiologyController {
+  constructor(private radiologyService: RadiologyService) {}
+
+  @Get()
+  getAll() {
+    return this.radiologyService.getAll();
+  }
+
+  @Get('id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.radiologyService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() body: RadiologyDto) {
+    return this.radiologyService.create(body);
+  }
+}
