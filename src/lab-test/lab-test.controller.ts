eab1e69fd232512d4f456c28851b23a29a37d65e
Sennay <wsennay@gmail.com>
2022-05-03 12:07:06 +0300

    Add Controller

---
+++ src/lab-test/lab-test.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('lab-test')
+export class LabTestController {}
--- src/lab-test/lab-test.module.ts
+++ src/lab-test/lab-test.module.ts
@@ -3,9 +3,11 @@ import { LabTestService } from './lab-test.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LabTest } from './labTest.entity';
 import { InvestigationRequestModule } from '../investigation-request/investigation-request.module';
+import { LabTestController } from './lab-test.controller';
 
 @Module({
   providers: [LabTestService],
   imports: [TypeOrmModule.forFeature([LabTest]), InvestigationRequestModule],
+  controllers: [LabTestController],
 })
 export class LabTestModule {}
