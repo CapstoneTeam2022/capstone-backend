f5f379acadd446050728f93a60587c79f92ef693
Sennay <wsennay@gmail.com>
2022-05-03 08:30:29 +0300

    Add Vitals Controller

--- src/vitals/vitals.module.ts
+++ src/vitals/vitals.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { VitalsService } from './vitals.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Vitals } from './vitals.entity';
+import { VitalsController } from './vitals.controller';
 
 @Module({
   providers: [VitalsService],
   imports: [TypeOrmModule.forFeature([Vitals])],
+  controllers: [VitalsController],
 })
 export class VitalsModule {}
