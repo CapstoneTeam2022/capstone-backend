18aeacc7116c1bc3b3ca5478ac4a3e9bc3b70a10
Sennay <wsennay@gmail.com>
2022-05-03 11:34:11 +0300

    add inv request service

--- src/investigation-request/investigation-request.service.ts
+++ src/investigation-request/investigation-request.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { InvestigationRequest } from './investigationRequest.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class InvestigationRequestService {}
+export class InvestigationRequestService {
+  constructor(
+    @InjectRepository(InvestigationRequest)
+    private investigationRequest: Repository<InvestigationRequest>,
+  ) {}
+
+  getAll(): Promise<InvestigationRequest[]> {
+    return this.investigationRequest.find();
+  }
+}
