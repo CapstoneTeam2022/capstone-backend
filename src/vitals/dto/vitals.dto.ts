097f01cda5d37a2a6b3cfcb73b382ab4f63429a3
Sennay <wsennay@gmail.com>
2022-05-03 07:58:42 +0300

    Add Vitals Dto

---
+++ src/vitals/dto/vitals.dto.ts
@@ -0,0 +1,27 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
+export class VitalsDto {
+  @IsNumber()
+  @IsNotEmpty()
+  temperature: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  pulse: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  respiratoryRate: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  bloodPressure: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  weight: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  spo2Level: number;
+}
