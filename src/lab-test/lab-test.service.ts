9056101fa1da460ef3d0e2b25fcd9fad99813244
Sennay <wsennay@gmail.com>
2022-05-03 12:06:19 +0300

    Add Create, Get and Get All

--- src/lab-test/lab-test.service.ts
+++ src/lab-test/lab-test.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LabTest } from './labTest.entity';
+import { Repository } from 'typeorm';
+import { LabTestDto } from './dto';
+import { InvestigationRequestService } from '../investigation-request/investigation-request.service';
 
 @Injectable()
-export class LabTestService {}
+export class LabTestService {
+  constructor(
+    @InjectRepository(LabTest)
+    private labTestRepository: Repository<LabTest>,
+    private invRequestService: InvestigationRequestService,
+  ) {}
+
+  getAllLabTest(): Promise<LabTest[]> {
+    return this.labTestRepository.find({ relations: ['investigationRequest'] });
+  }
+
+  async getLabTest(id: number) {
+    const labTest = await this.labTestRepository.findOne(id);
+    if (labTest) return labTest;
+    throw new NotFoundException(`The Lab test with id ${id} not found`);
+  }
+
+  async createLabTest({
+    investigationRequestId,
+    ...data
+  }: LabTestDto): Promise<LabTest> {
+    const investigationRequest =
+      await this.invRequestService.getInvestigationRequest(
+        investigationRequestId,
+      );
+    const labTest = this.labTestRepository.create({
+      ...data,
+      investigationRequest,
+    });
+    return this.labTestRepository.save(labTest);
+  }
+}
