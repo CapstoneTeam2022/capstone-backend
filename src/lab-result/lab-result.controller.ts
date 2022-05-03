f9778a2491ec6acd2460d7c3a4337a57d656d8aa
Sennay <wsennay@gmail.com>
2022-05-03 12:45:53 +0300

    Add Get One, Get All and Create

--- src/lab-result/lab-result.controller.ts
+++ src/lab-result/lab-result.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { LabResultService } from './lab-result.service';
+import { LabResultDto } from './dto';
 
 @Controller('lab-result')
-export class LabResultController {}
+export class LabResultController {
+  constructor(private labResultService: LabResultService) {}
+
+  @Get()
+  getAll() {
+    return this.labResultService.getAll();
+  }
+
+  @Get('id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.labResultService.getLabResult(id);
+  }
+
+  @Post()
+  create(@Body() body: LabResultDto) {
+    return this.labResultService.createLabResult(body);
+  }
+}
