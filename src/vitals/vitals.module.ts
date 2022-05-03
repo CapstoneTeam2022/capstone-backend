d125ca886b8ac329a7540f712de9c0160ccf5e68
Sennay <wsennay@gmail.com>
2022-05-03 08:52:28 +0300

    Import User and Patient Module

--- src/vitals/vitals.module.ts
+++ src/vitals/vitals.module.ts
@@ -3,10 +3,12 @@ import { VitalsService } from './vitals.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Vitals } from './vitals.entity';
 import { VitalsController } from './vitals.controller';
+import { UserModule } from '../user/user.module';
+import { PatientModule } from '../patient/patient.module';
 
 @Module({
   providers: [VitalsService],
-  imports: [TypeOrmModule.forFeature([Vitals])],
+  imports: [TypeOrmModule.forFeature([Vitals]), UserModule, PatientModule],
   controllers: [VitalsController],
 })
 export class VitalsModule {}
