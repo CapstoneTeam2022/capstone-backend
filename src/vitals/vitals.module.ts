5f647b2ed45d0399f8cbe20ce76580996e7b9fa3
Sennay <wsennay@gmail.com>
2022-05-03 08:20:54 +0300

    Add Vitals Service and Vitals Repo

--- src/vitals/vitals.module.ts
+++ src/vitals/vitals.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { VitalsService } from './vitals.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Vitals } from './vitals.entity';
 
-@Module({})
+@Module({
+  providers: [VitalsService],
+  imports: [TypeOrmModule.forFeature([Vitals])],
+})
 export class VitalsModule {}
