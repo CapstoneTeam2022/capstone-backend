d0b418c71da49ff32b454db427206568bf58c4a8
Sennay <wsennay@gmail.com>
2022-05-03 12:37:46 +0300

    Add Lab Result Repo

--- src/lab-result/lab-result.module.ts
+++ src/lab-result/lab-result.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LabResultService } from './lab-result.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LabResult } from './labResult.entity';
 
 @Module({
   providers: [LabResultService],
+  imports: [TypeOrmModule.forFeature([LabResult])],
 })
 export class LabResultModule {}
