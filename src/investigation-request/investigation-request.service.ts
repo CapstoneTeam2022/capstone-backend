df21ff06ad195b74df6278fc80710ec007c506d9
Sennay <wsennay@gmail.com>
2022-05-03 11:45:20 +0300

    Add Get One, Get All and Create

--- src/investigation-request/investigation-request.service.ts
+++ src/investigation-request/investigation-request.service.ts
@@ -1,16 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { InvestigationRequest } from './investigationRequest.entity';
 import { Repository } from 'typeorm';
+import { InvestigationRequestDto } from './dto';
+import { UserService } from '../user/user.service';
+import { VitalsService } from '../vitals/vitals.service';
 
 @Injectable()
 export class InvestigationRequestService {
   constructor(
     @InjectRepository(InvestigationRequest)
-    private investigationRequest: Repository<InvestigationRequest>,
+    private investigationRequestRepository: Repository<InvestigationRequest>,
+    private userService: UserService,
+    private vitalService: VitalsService,
   ) {}
 
   getAll(): Promise<InvestigationRequest[]> {
-    return this.investigationRequest.find();
+    return this.investigationRequestRepository.find();
+  }
+
+  async getInvestigationRequest(id: number): Promise<InvestigationRequest> {
+    const invRequest = await this.investigationRequestRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    if (invRequest) return invRequest;
+
+    throw new NotFoundException(
+      `Investigation Request with id ${id} not found`,
+    );
+  }
+
+  async createInvestigationRequest({
+    registeredById,
+    vitalId,
+    note,
+  }: InvestigationRequestDto): Promise<InvestigationRequest> {
+    const registeredBy = await this.userService.getUser(registeredById);
+    const vitals = await this.vitalService.getVital(vitalId);
+    const invRequest = this.investigationRequestRepository.create({
+      note,
+      registeredBy,
+      vitals,
+    });
+    return this.investigationRequestRepository.save(invRequest);
   }
 }
