b317915dfb71f366b9515b5ea3d25c27ab674799
Sennay <wsennay@gmail.com>
2022-05-03 11:17:53 +0300

    Add Investigation Request Repository

--- src/investigation-request/investigation-request.module.ts
+++ src/investigation-request/investigation-request.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InvestigationRequestService } from './investigation-request.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InvestigationRequest } from './investigationRequest.entity';
 
 @Module({
-  providers: [InvestigationRequestService]
+  providers: [InvestigationRequestService],
+  imports: [TypeOrmModule.forFeature([InvestigationRequest])],
 })
 export class InvestigationRequestModule {}
