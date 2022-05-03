670ff30bc934c0bf403f70bff574694aae0c32b2
Sennay <wsennay@gmail.com>
2022-05-03 13:26:49 +0300

    Import Modules

--- src/radiology/radiology.module.ts
+++ src/radiology/radiology.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { RadiologyService } from './radiology.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Radiology } from './radiology.entity';
+import { UserModule } from '../user/user.module';
+import { InvestigationRequestModule } from '../investigation-request/investigation-request.module';
 
 @Module({
   providers: [RadiologyService],
+  imports: [
+    TypeOrmModule.forFeature([Radiology]),
+    UserModule,
+    InvestigationRequestModule,
+  ],
 })
 export class RadiologyModule {}
