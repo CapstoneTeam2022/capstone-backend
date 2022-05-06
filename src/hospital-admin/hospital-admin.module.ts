fadb6a613e9efc800552b288be144dc2eef76b32
Sennay <wsennay@gmail.com>
2022-05-06 13:27:34 +0300

    Import role module

--- src/hospital-admin/hospital-admin.module.ts
+++ src/hospital-admin/hospital-admin.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { HospitalAdminService } from './hospital-admin.service';
 import { HospitalAdminController } from './hospital-admin.controller';
 import { UserModule } from '../user/user.module';
+import { RoleModule } from '../role/role.module';
 
 @Module({
   providers: [HospitalAdminService],
   controllers: [HospitalAdminController],
-  imports: [UserModule],
+  imports: [UserModule, RoleModule],
 })
 export class HospitalAdminModule {}
