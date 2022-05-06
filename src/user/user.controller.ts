ea129c876646d71a254ee3c98f0f5e71d16a8366
Sennay <wsennay@gmail.com>
2022-05-06 12:56:12 +0300

    Add user CRUD

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserWithRoleDto } from './dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  getAll() {
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.getUser(id);
+  }
+
+  @Post()
+  create(@Body() { role, ...body }: CreateUserWithRoleDto) {
+    return this.userService.addUser(body, role);
+  }
+}
