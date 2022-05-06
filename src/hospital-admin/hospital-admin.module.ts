13d322a87effe35287699eaf5ed6edfbb64a71e2
Sennay <wsennay@gmail.com>
2022-05-06 13:25:32 +0300

    Import user module

--- src/hospital-admin/hospital-admin.module.ts
+++ src/hospital-admin/hospital-admin.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HospitalAdminService } from './hospital-admin.service';
 import { HospitalAdminController } from './hospital-admin.controller';
+import { UserModule } from '../user/user.module';
 
 @Module({
   providers: [HospitalAdminService],
   controllers: [HospitalAdminController],
+  imports: [UserModule],
 })
 export class HospitalAdminModule {}
