7a9192f0f6eb4e8b53fc0161c4376295d009d2a2
Sennay <wsennay@gmail.com>
2022-05-06 15:31:56 +0300

    Add create and read

--- src/hospital-admin/hospital-admin.service.ts
+++ src/hospital-admin/hospital-admin.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { RoleService } from '../role/role.service';
+import { UserDto } from '../user/dto';
 
 @Injectable()
-export class HospitalAdminService {}
+export class HospitalAdminService {
+  readonly roleName = 'Hospital Admin';
+  constructor(private userService: UserService) {}
+
+  getAll() {
+    return this.userService.findAllByRoleName(this.roleName);
+  }
+
+  getById(id: number) {
+    return this.userService.findOneByRoleName(id, this.roleName);
+  }
+
+  createHospitalAdmin(user: UserDto) {
+    return this.userService.addUser(user, this.roleName);
+  }
+}
