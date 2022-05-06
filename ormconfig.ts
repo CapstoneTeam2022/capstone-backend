30f739c73677f19ae125be3a2bf3bafcfb1c14ed
Sennay <wsennay@gmail.com>
2022-05-06 11:57:25 +0300

    setup ssl for dev and prod

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,34 +1,6 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import 'dotenv/config';
 
-//Check if ssl is enabled
-const sslVal = process.env.ENABLE_SSL;
-console.log('Value', sslVal, typeof sslVal);
-let isSSlEnabled = false;
-if (sslVal === 'true') {
-  isSSlEnabled = true;
-}
-
-const ssl = isSSlEnabled ? { rejectUnauthorized: false } : false;
-
-// if (
-//   isSSlEnabled === undefined ||
-//   isSSlEnabled === null ||
-//   isSSlEnabled === false
-// ) {
-//   isSSlEnabled = false;
-// } else {
-//   isSSlEnabled = true;
-// }
-//
-// const ssl = isSSlEnabled
-//   ? {
-//       rejectUnauthorized: false,
-//     }
-//   : false;
-//
-// console.log(isSSlEnabled, ssl);
-
 const dbConfig: PostgresConnectionOptions = {
   type: 'postgres',
   username: process.env.POSTGRES_USER,
