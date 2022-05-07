eda7153dec93fdc67c12b46a47766e91939c3786
Sennay <wsennay@gmail.com>
2022-05-07 21:02:40 +0300

    Import User Service

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { EmployeeController } from './employee.controller';
+import { UserService } from '../user/user.service';
 
 @Module({
   controllers: [EmployeeController],
   providers: [EmployeeService],
+  imports: [UserService],
 })
 export class EmployeeModule {}
