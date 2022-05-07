c15bfde0c8850145f84347d94414532e29ab85d3
Sennay <wsennay@gmail.com>
2022-05-07 21:21:32 +0300

    Change User Service to User Module

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { EmployeeController } from './employee.controller';
 import { UserService } from '../user/user.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
   controllers: [EmployeeController],
   providers: [EmployeeService],
-  imports: [UserService],
+  imports: [UserModule],
 })
 export class EmployeeModule {}
