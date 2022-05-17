84cc6007bca0568d6d996882c62ddac7935166b1
Sennay <wsennay@gmail.com>
2022-05-17 13:55:34 +0300

    Import user module and typeorm module

--- src/moh-employee/moh-employee.module.ts
+++ src/moh-employee/moh-employee.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MohEmployeeService } from './moh-employee.service';
 import { MohEmployeeController } from './moh-employee.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MohEmployee } from './entities/moh-employee.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MohEmployee]), UserModule],
   controllers: [MohEmployeeController],
-  providers: [MohEmployeeService]
+  providers: [MohEmployeeService],
 })
 export class MohEmployeeModule {}
