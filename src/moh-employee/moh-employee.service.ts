c34d423d918614cd797fd28af08bec19e7ac5950
Sennay <wsennay@gmail.com>
2022-05-17 14:05:23 +0300

    Create Service

--- src/moh-employee/moh-employee.service.ts
+++ src/moh-employee/moh-employee.service.ts
@@ -1,19 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMohEmployeeDto } from './dto/create-moh-employee.dto';
 import { UpdateMohEmployeeDto } from './dto/update-moh-employee.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MohEmployee } from './entities/moh-employee.entity';
+import { Repository } from 'typeorm';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class MohEmployeeService {
-  create(createMohEmployeeDto: CreateMohEmployeeDto) {
-    return 'This action adds a new mohEmployee';
+  constructor(
+    @InjectRepository(MohEmployee)
+    private mohEmployeeRepository: Repository<MohEmployee>,
+    private userService: UserService,
+  ) {}
+
+  async create({ user, registeredBy, ...data }: CreateMohEmployeeDto) {
+    const newUser = await this.userService.addUser(user, 'MohEmployee');
+    const registerer = await this.userService.getUser(registeredBy);
+    const mohEmployee = this.mohEmployeeRepository.create({
+      ...data,
+      user: newUser,
+      registeredBy: registerer,
+    });
+    return this.mohEmployeeRepository.save(mohEmployee);
   }
 
   findAll() {
-    return `This action returns all mohEmployee`;
+    return this.mohEmployeeRepository.find({
+      relations: ['user'],
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} mohEmployee`;
+  async findOne(id: number) {
+    const mohEmployee = await this.mohEmployeeRepository.findOne({
+      where: {
+        id,
+      },
+      relations: ['user'],
+    });
+    if (mohEmployee) return mohEmployee;
+    throw new NotFoundException(`MohEmployee with id ${id} not found`);
   }
 
   update(id: number, updateMohEmployeeDto: UpdateMohEmployeeDto) {
