95470a371306c1073e83998c453f7c4a7afa5b5e
Sennay <wsennay@gmail.com>
2022-05-17 14:03:57 +0300

    Generate MOH Employee Entity

--- src/moh-employee/entities/moh-employee.entity.ts
+++ src/moh-employee/entities/moh-employee.entity.ts
@@ -1,1 +1,29 @@
-export class MohEmployee {}
+import { User } from 'src/user/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToMany,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class MohEmployee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  emergencyContactName: string;
+
+  @Column()
+  emergencyContactPhone: string;
+
+  @OneToOne(() => User, (user) => user.mohEmployee)
+  @JoinColumn()
+  user: User;
+
+  @ManyToOne(() => User, (user) => user.registeredMohEmployees)
+  registeredBy: User;
+}
