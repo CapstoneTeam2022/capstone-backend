d4f4c3c9444127f4f51e40e1c4191bef04e8e7f3
Sennay <wsennay@gmail.com>
2022-05-18 17:19:48 +0300

    add diagnosis

--- src/diagnosis/diagnosis.controller.ts
+++ src/diagnosis/diagnosis.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { DiagnosisService } from './diagnosis.service';
 import { CreateDiagnosisDto } from './dto/create-diagnosis.dto';
 import { UpdateDiagnosisDto } from './dto/update-diagnosis.dto';
@@ -22,13 +30,13 @@ export class DiagnosisController {
     return this.diagnosisService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDiagnosisDto: UpdateDiagnosisDto) {
-    return this.diagnosisService.update(+id, updateDiagnosisDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.diagnosisService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateDiagnosisDto: UpdateDiagnosisDto) {
+  //   return this.diagnosisService.update(+id, updateDiagnosisDto);
+  // }
+  //
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.diagnosisService.remove(+id);
+  // }
 }
--- src/diagnosis/entities/diagnosis.entity.ts
+++ src/diagnosis/entities/diagnosis.entity.ts
@@ -1,1 +1,33 @@
-export class Diagnosis {}
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  OneToMany,
+} from 'typeorm';
+import { User } from '../../user/user.entity';
+import { InvestigationRequest } from '../../investigation-request/investigationRequest.entity';
+
+//@Entity()
+export class Diagnosis {
+  // @PrimaryGeneratedColumn()
+  // id: number;
+  //
+  // @Column()
+  // comment: string;
+  //
+  // @Column()
+  // measuredIn: string;
+  //
+  // // @OneToMany(() => Disease, (disease) => disease.diagnosis)
+  // // disease: Disease[];
+  //
+  // @ManyToOne(() => User, (user) => user.diagnosis)
+  // filledBy: User;
+  //
+  // @ManyToOne(
+  //   () => InvestigationRequest,
+  //   (investigationReq) => investigationReq.diagnosis,
+  // )
+  // investigationRequest: InvestigationRequest;
+}
