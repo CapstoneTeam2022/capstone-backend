caa3c17ee74eba4e19fd922d1578686336c956d3
Sennay <wsennay@gmail.com>
2022-05-18 20:19:35 +0300

    Import Modules

--- src/diagnosis/diagnosis.module.ts
+++ src/diagnosis/diagnosis.module.ts
@@ -3,9 +3,21 @@ import { DiagnosisService } from './diagnosis.service';
 import { DiagnosisController } from './diagnosis.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Diagnosis } from './entities/diagnosis.entity';
+import { UserModule } from '../user/user.module';
+import { InvestigationRequestModule } from '../investigation-request/investigation-request.module';
+import { DiseaseModule } from '../disease/disease.module';
+import { PatientModule } from '../patient/patient.module';
+import { VitalsModule } from '../vitals/vitals.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Diagnosis])],
+  imports: [
+    TypeOrmModule.forFeature([Diagnosis]),
+    UserModule,
+    InvestigationRequestModule,
+    DiseaseModule,
+    PatientModule,
+    VitalsModule,
+  ],
   controllers: [DiagnosisController],
   providers: [DiagnosisService],
 })
