74259070dfbee2834f826bb3123f9377319caab4
Sennay <wsennay@gmail.com>
2022-05-18 19:05:49 +0300

    Add diagnosis repo

--- src/diagnosis/diagnosis.module.ts
+++ src/diagnosis/diagnosis.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DiagnosisService } from './diagnosis.service';
 import { DiagnosisController } from './diagnosis.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Diagnosis } from './entities/diagnosis.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Diagnosis])],
   controllers: [DiagnosisController],
   providers: [DiagnosisService],
 })
