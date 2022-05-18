86ea4d6c9d5000c6da47ff3a63788b7dab24a249
Sennay <wsennay@gmail.com>
2022-05-18 18:55:31 +0300

    Create disease dto

--- src/disease/dto/create-disease.dto.ts
+++ src/disease/dto/create-disease.dto.ts
@@ -1,1 +1,11 @@
-export class CreateDiseaseDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateDiseaseDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+}
