a0195f27a17631313019eadfb13ae9551242beaa
Sennay <wsennay@gmail.com>
2022-05-18 20:20:01 +0300

    Add dto

--- src/diagnosis/dto/create-diagnosis.dto.ts
+++ src/diagnosis/dto/create-diagnosis.dto.ts
@@ -1,1 +1,22 @@
-export class CreateDiagnosisDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateDiagnosisDto {
+  @IsNotEmpty()
+  @IsString()
+  comment: string;
+
+  @IsNotEmpty()
+  @IsString()
+  measuredIn: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  filledById: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  investigationRequestId: number;
+
+  @IsNumber({}, { each: true })
+  diseases: number[];
+}
