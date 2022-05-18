39dc7e14c626fb15d55cd2e6a0e9dee85fdc72ae
Sennay <wsennay@gmail.com>
2022-05-18 19:04:07 +0300

    Add disease repo

--- src/disease/disease.module.ts
+++ src/disease/disease.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DiseaseService } from './disease.service';
 import { DiseaseController } from './disease.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Disease } from './entities/disease.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Disease])],
   controllers: [DiseaseController],
-  providers: [DiseaseService]
+  providers: [DiseaseService],
 })
 export class DiseaseModule {}
