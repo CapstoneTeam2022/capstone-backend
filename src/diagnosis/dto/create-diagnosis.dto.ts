75668c49e3855802e6f7fcbed4fd75a71b984f3c
Sennay <wsennay@gmail.com>
2022-05-19 12:44:01 +0300

    Remove measured in

--- src/diagnosis/dto/create-diagnosis.dto.ts
+++ src/diagnosis/dto/create-diagnosis.dto.ts
@@ -5,10 +5,6 @@ export class CreateDiagnosisDto {
   @IsString()
   comment: string;
 
-  @IsNotEmpty()
-  @IsString()
-  measuredIn: string;
-
   @IsNumber()
   @IsNotEmpty()
   filledById: number;
