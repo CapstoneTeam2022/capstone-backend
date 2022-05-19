0a5e84ce6209bf61c8e6b186bd19f0a967473d9a
Sennay <wsennay@gmail.com>
2022-05-19 14:48:50 +0300

    Import modules

--- src/prescription/prescription.module.ts
+++ src/prescription/prescription.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrescriptionService } from './prescription.service';
 import { PrescriptionController } from './prescription.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Prescription } from './entities/prescription.entity';
+import { DiagnosisModule } from '../diagnosis/diagnosis.module';
 
 @Module({
   controllers: [PrescriptionController],
   providers: [PrescriptionService],
+  imports: [TypeOrmModule.forFeature([Prescription]), DiagnosisModule],
 })
 export class PrescriptionModule {}
