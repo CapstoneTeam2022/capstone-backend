ff0f83aa730c76203abc488797dd385241812090
Sennay <wsennay@gmail.com>
2022-05-19 14:48:10 +0300

    Add prescription relation

--- src/diagnosis/entities/diagnosis.entity.ts
+++ src/diagnosis/entities/diagnosis.entity.ts
@@ -4,12 +4,14 @@ import {
   Entity,
   ManyToMany,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { User } from '../../user/user.entity';
 import { InvestigationRequest } from '../../investigation-request/investigationRequest.entity';
 import { Disease } from '../../disease/entities/disease.entity';
 import { JoinTable } from 'typeorm';
+import { Prescription } from '../../prescription/entities/prescription.entity';
 
 @Entity()
 export class Diagnosis {
@@ -34,4 +36,7 @@ export class Diagnosis {
   @ManyToMany(() => Disease)
   @JoinTable({ name: 'DiagnosisDisease' }) //Join table name will be 'DiagnosisDisease'
   diseases: Disease[];
+
+  @OneToMany(() => Prescription, (prescription) => prescription.diagnosis)
+  prescriptions: Prescription[];
 }
