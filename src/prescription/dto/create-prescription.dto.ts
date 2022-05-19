ef013f55913951e10d5c458688ee51f75ed89da8
Sennay <wsennay@gmail.com>
2022-05-19 14:47:52 +0300

    Add dto items

--- src/prescription/dto/create-prescription.dto.ts
+++ src/prescription/dto/create-prescription.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePrescriptionDto {}
+import { IsArray, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreatePrescriptionDto {
+  @IsNotEmpty()
+  @IsString()
+  comment: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  diagnosisId: number;
+
+  @IsArray()
+  @IsString({ each: true })
+  medications: string[];
+}
