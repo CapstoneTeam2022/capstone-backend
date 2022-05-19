37229dc5a2f803837970e2a9901f248e46536b50
Sennay <wsennay@gmail.com>
2022-05-19 14:48:38 +0300

    Add entity columns

--- src/prescription/entities/prescription.entity.ts
+++ src/prescription/entities/prescription.entity.ts
@@ -1,1 +1,26 @@
-export class Prescription {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Diagnosis } from '../../diagnosis/entities/diagnosis.entity';
+
+@Entity()
+export class Prescription {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  comment: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column('text', { array: true })
+  medications: string[];
+
+  @ManyToOne(() => Diagnosis, (diagnosis) => diagnosis.prescriptions)
+  diagnosis: Diagnosis;
+}
