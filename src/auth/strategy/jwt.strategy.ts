728a22e10d32bded1797303c9cec95f15d163d87
Sennay <wsennay@gmail.com>
2022-05-24 15:41:32 +0300

    Add jwt strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private authService: AuthService, config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: Payload) {
+    const user = await this.authService.findById(payload.sub);
+    console.log('User in validate function', user);
+    return {
+      ...user,
+      role: user.role.name,
+    };
+  }
+}
+
+type Payload = {
+  sub: number;
+  email: string;
+  role: string;
+};
