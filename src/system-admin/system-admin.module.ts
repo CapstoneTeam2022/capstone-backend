d27ab0f38d6d0a99c75144dfc10abc12d96cf88a
Sennay <wsennay@gmail.com>
2022-05-31 15:27:06 +0300

    Generate system admin service

--- src/system-admin/system-admin.module.ts
+++ src/system-admin/system-admin.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SystemAdminController } from './system-admin.controller';
+import { SystemAdminService } from './system-admin.service';
 
 @Module({
-  controllers: [SystemAdminController]
+  controllers: [SystemAdminController],
+  providers: [SystemAdminService],
 })
 export class SystemAdminModule {}
