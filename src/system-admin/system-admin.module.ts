3a247189367407b21a7f03b93302a986de8d6a0a
Sennay <wsennay@gmail.com>
2022-05-31 17:14:10 +0300

    Import deps

--- src/system-admin/system-admin.module.ts
+++ src/system-admin/system-admin.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { SystemAdminController } from './system-admin.controller';
 import { SystemAdminService } from './system-admin.service';
+import { DiagnosisModule } from '../diagnosis/diagnosis.module';
+import { HealthCenterModule } from '../health-center/health-center.module';
+import { ResearcherModule } from '../researcher/researcher.module';
+import { PatientModule } from '../patient/patient.module';
+import { InvestigationRequestModule } from '../investigation-request/investigation-request.module';
+import { MohEmployeeModule } from '../moh-employee/moh-employee.module';
+import { LabResultModule } from '../lab-result/lab-result.module';
+import { RadiologyModule } from '../radiology/radiology.module';
 
 @Module({
   controllers: [SystemAdminController],
   providers: [SystemAdminService],
+  imports: [
+    DiagnosisModule,
+    HealthCenterModule,
+    ResearcherModule,
+    PatientModule,
+    InvestigationRequestModule,
+    MohEmployeeModule,
+    LabResultModule,
+    RadiologyModule,
+    //  ResearcherModule,
+  ],
 })
 export class SystemAdminModule {}
