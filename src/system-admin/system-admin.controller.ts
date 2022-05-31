d8e8b53695c2727af9b443f888835516d4d6adfb
Sennay <wsennay@gmail.com>
2022-05-31 17:14:21 +0300

    add get report route

--- src/system-admin/system-admin.controller.ts
+++ src/system-admin/system-admin.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ReportDto } from './dto';
+import { SystemAdminService } from './system-admin.service';
 
 @Controller('system-admin')
-export class SystemAdminController {}
+export class SystemAdminController {
+  constructor(private readonly systemAdminService: SystemAdminService) {}
+  @Post()
+  getReport(@Body() report: ReportDto) {
+    return this.systemAdminService.getReport(report);
+  }
+}
