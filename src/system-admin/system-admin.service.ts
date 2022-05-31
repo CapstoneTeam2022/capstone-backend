b1006f9cb7c19fcc68269ff689c1117cea6ef530
Sennay <wsennay@gmail.com>
2022-05-31 20:02:06 +0300

    Import user service

--- src/system-admin/system-admin.service.ts
+++ src/system-admin/system-admin.service.ts
@@ -8,6 +8,7 @@ import { InvestigationRequestService } from '../investigation-request/investigat
 import { MohEmployeeService } from '../moh-employee/moh-employee.service';
 import { LabResultService } from '../lab-result/lab-result.service';
 import { RadiologyService } from '../radiology/radiology.service';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class SystemAdminService {
@@ -20,30 +21,38 @@ export class SystemAdminService {
     private readonly mohEmployeeService: MohEmployeeService,
     private readonly labResultService: LabResultService,
     private readonly radiologyService: RadiologyService,
+    private readonly userService: UserService,
   ) {}
 
-  async getReport(report: ReportDto) {
+  async getReport({ start, end, information }: ReportDto) {
     const items = {};
-    for (const item of report.information) {
+    for (const item of information) {
       if (item === ReportInfo.HOSPITAL) {
         items[ReportInfo.HOSPITAL] =
-          await this.healthCenterService.getAllHealthCenters(); // include employee count;
+          await this.healthCenterService.getAllInDateRange(start, end); //TODO include employee count;
       } else if (item === ReportInfo.RESEARCHER) {
-        items[ReportInfo.RESEARCHER] = await this.researcherService.getAll();
+        items[ReportInfo.RESEARCHER] =
+          await this.researcherService.getAllInDateRange(start, end);
       } else if (item === ReportInfo.PATIENT) {
-        items[ReportInfo.PATIENT] = await this.patientService.getAllPatients();
+        items[ReportInfo.PATIENT] = await this.patientService.getAllInDateRange(
+          start,
+          end,
+        );
       } else if (item === ReportInfo.INVESTIGATION_REQUEST) {
         items[ReportInfo.INVESTIGATION_REQUEST] =
-          await this.investigationRequestService.getAll();
+          await this.investigationRequestService.getAllInDateRange(start, end);
       } else if (item === ReportInfo.MOH_EMPLOYEE) {
         items[ReportInfo.MOH_EMPLOYEE] =
-          await this.mohEmployeeService.findAll();
+          await this.mohEmployeeService.getAllInDateRange(start, end);
       } else if (item === ReportInfo.LAB_RESULT) {
-        items[ReportInfo.LAB_RESULT] = await this.labResultService.getAll();
+        items[ReportInfo.LAB_RESULT] =
+          await this.labResultService.getAllInDateRange(start, end);
       } else if (item === ReportInfo.RADIOLOGY) {
-        items[ReportInfo.RADIOLOGY] = await this.radiologyService.getAll();
+        items[ReportInfo.RADIOLOGY] =
+          await this.radiologyService.getAllInDateRange(start, end);
       } else if (item === ReportInfo.DIAGNOSIS) {
-        items[ReportInfo.DIAGNOSIS] = await this.diagnosisService.findAll();
+        items[ReportInfo.DIAGNOSIS] =
+          await this.diagnosisService.getAllInDateRange(start, end);
       }
     }
     return items;
