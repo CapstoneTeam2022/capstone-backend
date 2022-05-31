68d0130a1c9ae522580ee556256e2094ec923197
Sennay <wsennay@gmail.com>
2022-05-31 22:03:37 +0300

    add get report

--- src/system-admin/system-admin.controller.ts
+++ src/system-admin/system-admin.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { ReportDto } from './dto';
 import { SystemAdminService } from './system-admin.service';
+import { Response } from 'express';
 
 @Controller('system-admin')
 export class SystemAdminController {
@@ -8,5 +9,18 @@ export class SystemAdminController {
   @Post()
   getReport(@Body() report: ReportDto) {
     return this.systemAdminService.getReport(report);
+  }
+
+  @Post('/report')
+  async getPDF(@Res() res: Response, @Body() report: ReportDto): Promise<void> {
+    const buffer = await this.systemAdminService.generatePDF(report);
+
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'inline; filename=example.pdf',
+      'Content-Length': buffer.length,
+    });
+
+    res.end(buffer);
   }
 }
