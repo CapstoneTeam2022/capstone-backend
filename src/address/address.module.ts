3e050440b3515fc91178d4eb11d0336c839c4e07
Sennay <wsennay@gmail.com>
2022-06-01 15:23:59 +0300

    add address controller

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Address } from './address.entity';
+import { AddressController } from './address.controller';
 
 @Module({
   providers: [AddressService],
   imports: [TypeOrmModule.forFeature([Address])],
   exports: [AddressService],
+  controllers: [AddressController],
 })
 export class AddressModule {}
