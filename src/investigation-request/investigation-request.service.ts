9c0023a2f12dd952714924522013eba4c5b4e6d9
Sennay <wsennay@gmail.com>
2022-06-22 19:36:19 +0300

    fix inv requests

--- src/investigation-request/investigation-request.service.ts
+++ src/investigation-request/investigation-request.service.ts
@@ -160,16 +160,12 @@ export class InvestigationRequestService {
       .innerJoinAndSelect('vital.requestedBy', 'user')
       .innerJoinAndSelect('user.healthCenter', 'h')
       .innerJoinAndSelect('inv.labTests', 'tests')
+      .innerJoinAndSelect('inv.labResults', 'res')
       .where('h.id=:hid', { hid: healthCenterId })
       .andWhere('inv.remainingTests > :num', { num: 0 })
       .select(['inv', 'tests'])
       .orderBy('inv.createdAt', 'DESC')
       .getMany();
-
-    // for (const request of requests) {
-    //   const results = await this.getAllForInvestigationRequest(request.id);
-    //   request.labResults = results;
-    // }
 
     return requests;
   }
